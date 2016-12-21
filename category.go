310a71bee8ea2494babd28e6bf7a253417e5f45b
Cristian Echeverría <cer1969@gmail.com>
2016-12-21 15:56:34 -0300

    Reorganizaci?n de c?digo

---
+++ category.go
@@ -0,0 +1,80 @@
+// Copyright Cristian Echeverría Rabí
+
+package conductor
+
+import (
+	"bitbucket.org/tormundo/go.value/checker"
+)
+
+//----------------------------------------------------------------------------------------
+
+// CategoryArgs Container for arguments for Category constructor
+// Is a mutable version of a Category objects that allow to change attributes
+type CategoryArgs struct {
+	Name    string
+	Modelas float64
+	Coefexp float64
+	Creep   float64
+	Alpha   float64
+	Id      string
+}
+
+//----------------------------------------------------------------------------------------
+
+// NewCategory Returns Category object from attributes values
+// name string     : Name of conductor category
+// modelas float64 : Modulus of elasticity [kg/mm2] (required modelas > 0)
+// coefexp float64 : Coefficient of Thermal Expansion [1/°C] (required coefexp > 0)
+// creep float64   : Creep °C (required creep >= 0)
+// alpha float64   : Temperature coefficient of resistance [1/°C] (required 0 < alpha < 1)
+// id string       : Database id
+func NewCategory(name string, modelas float64, coefexp float64, creep float64, alpha float64, id string) (*Category, error) {
+	vc := checker.New("NewCategory")
+	vc.Ck("Modelas", modelas).Gt(0.0)
+	vc.Ck("Coefexp", coefexp).Gt(0.0)
+	vc.Ck("Creep", creep).Ge(0.0)
+	vc.Ck("Alpha", alpha).Gt(0.0).Lt(1.0)
+	err := vc.Error()
+	if err != nil {
+		return nil, err
+	}
+	return &Category{name, modelas, coefexp, creep, alpha, id}, nil
+}
+
+// NewCategoryFromArgs Returns Category object from CategoryArgs object
+func NewCategoryFromArgs(ca CategoryArgs) (*Category, error) {
+	vc := checker.New("NewCategoryFromArgs")
+	vc.Ck("Modelas", ca.Modelas).Gt(0.0)
+	vc.Ck("Coefexp", ca.Coefexp).Gt(0.0)
+	vc.Ck("Creep", ca.Creep).Ge(0.0)
+	vc.Ck("Alpha", ca.Alpha).Gt(0.0).Lt(1.0)
+	err := vc.Error()
+	if err != nil {
+		return nil, err
+	}
+	return &Category{ca.Name, ca.Modelas, ca.Coefexp, ca.Creep, ca.Alpha, ca.Id}, nil
+}
+
+//----------------------------------------------------------------------------------------
+
+// Category Container for category characteristics. Groups similar conductors.
+type Category struct {
+	Name    string  // Name of conductor category
+	Modelas float64 // Modulus of elasticity [kg/mm2]
+	Coefexp float64 // Coefficient of Thermal Expansion [1/°C]
+	Creep   float64 // Creep °C
+	Alpha   float64 // Temperature coefficient of resistance [1/°C]
+	Id      string  // Optional database id
+}
+
+// Category instances to use as constants
+var (
+	CC_CU     = Category{"COPPER", 12000.0, 0.0000169, 0.0, 0.00374, "CU"}
+	CC_AAAC   = Category{"AAAC (AASC)", 6450.0, 0.0000230, 20.0, 0.00340, "AAAC"}
+	CC_ACAR   = Category{"ACAR", 6450.0, 0.0000250, 20.0, 0.00385, "ACAR"}
+	CC_ACSR   = Category{"ACSR", 8000.0, 0.0000191, 20.0, 0.00395, "ACSR"}
+	CC_AAC    = Category{"ALUMINUM", 5600.0, 0.0000230, 20.0, 0.00395, "AAC"}
+	CC_CUWELD = Category{"COPPERWELD", 16200.0, 0.0000130, 0.0, 0.00380, "CUWELD"}
+	CC_AASC   = CC_AAAC
+	CC_ALL    = CC_AAC
+)
--- conductor.go
+++ conductor.go
@@ -1,29 +1,8 @@
 // Copyright Cristian Echeverría Rabí
 
-// Calculations for high voltage conductors
 package conductor
 
-// Constants values
-const (
-	TA_MIN      = -90.0     // Minumum value for ambient temperature (World lowest -82.2°C Vostok Antartica 21/07/1983)
-	TA_MAX      = 90.0      // Maximum value for ambient temperature (World highest 58.2°C Libia 13/09/1922)
-	TC_MIN      = -90.0     // Minumum value for conductor temperature
-	TC_MAX      = 2000.0    // Maximum value for conductor temperature (Copper melt at 1083 °C)
-	ITER_MAX    = 20000     // Maximun iterations number
-	TENSION_MAX = 50000.0   // Maximun condutor tension
-	CF_CLASSIC  = "CLASSIC" // CLASSIC Formula
-	CF_IEEE     = "IEEE"    // IEEE Formula
-)
-
-// Category Container for category characteristics. Groups similar conductors.
-type Category struct {
-	Name    string  // Name of conductor category
-	Modelas float64 // Modulus of elasticity [kg/mm2]
-	Coefexp float64 // Coefficient of Thermal Expansion [1/°C]
-	Creep   float64 // Creep °C
-	Alpha   float64 // Temperature coefficient of resistance [1/°C]
-	Id      string  // Optional database id
-}
+//----------------------------------------------------------------------------------------
 
 // Conductor Container for conductor characteristics
 type Conductor struct {
@@ -37,15 +16,3 @@ type Conductor struct {
 	Hcap     float64 // Heat capacity [kcal/(ft*°C)]
 	Id       string  // Optional database id
 }
-
-// Category instances to use as constants
-var (
-	CC_CU     = Category{"COPPER", 12000.0, 0.0000169, 0.0, 0.00374, "CU"}
-	CC_AAAC   = Category{"AAAC (AASC)", 6450.0, 0.0000230, 20.0, 0.00340, "AAAC"}
-	CC_ACAR   = Category{"ACAR", 6450.0, 0.0000250, 20.0, 0.00385, "ACAR"}
-	CC_ACSR   = Category{"ACSR", 8000.0, 0.0000191, 20.0, 0.00395, "ACSR"}
-	CC_AAC    = Category{"ALUMINUM", 5600.0, 0.0000230, 20.0, 0.00395, "AAC"}
-	CC_CUWELD = Category{"COPPERWELD", 16200.0, 0.0000130, 0.0, 0.00380, "CUWELD"}
-	CC_AASC   = CC_AAAC
-	CC_ALL    = CC_AAC
-)
