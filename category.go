b1cbfb627f8ba628ab4f403399cef4615fbf8c85
Cristian Echeverr?a <cer1969@gmail.com>
2016-12-26 00:04:34 -0300

    Category exported con unexported fields. Nueva versi?n de Conductor.

--- category.go
+++ category.go
@@ -3,13 +3,15 @@
 package conductor
 
 import (
+	"fmt"
+
 	"bitbucket.org/tormundo/go.value/checker"
 )
 
 //----------------------------------------------------------------------------------------
 
 // CategoryArgs Container for arguments for Category constructor
-// Is a mutable version of a Category objects that allow to change attributes
+// Is a mutable version of a Category object that allow to change attributes
 type CategoryArgs struct {
 	Name    string
 	Modelas float64
@@ -19,78 +21,104 @@ type CategoryArgs struct {
 	Id      string
 }
 
-// Get Returns Category object from attributes values
-func (ca *CategoryArgs) Get() (*category, error) {
-	vc := checker.New("CategoryArgs Get")
-	cat, err := NewCategory(ca.Name, ca.Modelas, ca.Coefexp, ca.Creep, ca.Alpha, ca.Id)
-	if err != nil {
-		vc.AppendSub(err)
-		return nil, vc.Error()
+// Get Returns *category object from attributes values
+func (ca *CategoryArgs) Get() (*Category, error) {
+	vc := checker.New(fmt.Sprintf("CategoryArgs Get %s", ca.Name))
+	cat, errsub := NewCategory(ca.Name, ca.Modelas, ca.Coefexp, ca.Creep, ca.Alpha, ca.Id)
+	vc.AppendSub(errsub)
+	if err := vc.Error(); err != nil {
+		return nil, err
 	}
 	return cat, nil
 }
 
 //----------------------------------------------------------------------------------------
 
-// NewCategory Returns Category object from attributes values
-// name string     : Name of conductor category
+// NewCategory Returns *category object from arguments values
+// name    string  : Name of conductor category
 // modelas float64 : Modulus of elasticity [kg/mm2] (required modelas > 0)
 // coefexp float64 : Coefficient of Thermal Expansion [1/°C] (required coefexp > 0)
-// creep float64   : Creep °C (required creep >= 0)
-// alpha float64   : Temperature coefficient of resistance [1/°C] (required 0 < alpha < 1)
-// id string       : Database id
-func NewCategory(name string, modelas float64, coefexp float64, creep float64, alpha float64, id string) (*category, error) {
-	vc := checker.New("NewCategory")
+// creep   float64 : Creep °C (required creep >= 0)
+// alpha   float64 : Temperature coefficient of resistance [1/°C] (required 0 < alpha < 1)
+// id      string  : Database id
+func NewCategory(name string, modelas float64, coefexp float64, creep float64,
+	alpha float64, id string) (*Category, error) {
+
+	vc := checker.New(fmt.Sprintf("NewCategory %s", name))
 	vc.Ck("Modelas", modelas).Gt(0.0)
 	vc.Ck("Coefexp", coefexp).Gt(0.0)
 	vc.Ck("Creep", creep).Ge(0.0)
 	vc.Ck("Alpha", alpha).Gt(0.0).Lt(1.0)
-	err := vc.Error()
-	if err != nil {
+	if err := vc.Error(); err != nil {
 		return nil, err
 	}
-	return &category{name, modelas, coefexp, creep, alpha, id}, nil
+	return &Category{name, modelas, coefexp, creep, alpha, id}, nil
 }
 
 //----------------------------------------------------------------------------------------
 
 // Category Container for category characteristics. Groups similar conductors.
-type category struct {
+type Category struct {
 	name    string  // Name of conductor category
 	modelas float64 // Modulus of elasticity [kg/mm2]
 	coefexp float64 // Coefficient of Thermal Expansion [1/°C]
 	creep   float64 // Creep °C
 	alpha   float64 // Temperature coefficient of resistance [1/°C]
-	id      string  // Optional database id
+	id      string  // Database id
 }
 
-func (cat *category) Name() string {
+func (cat *Category) Name() string {
 	return cat.name
 }
-func (cat *category) Modelas() float64 {
+func (cat *Category) Modelas() float64 {
 	return cat.modelas
 }
-func (cat *category) Coefexp() float64 {
+func (cat *Category) Coefexp() float64 {
 	return cat.coefexp
 }
-func (cat *category) Creep() float64 {
+func (cat *Category) Creep() float64 {
 	return cat.creep
 }
-func (cat *category) Alpha() float64 {
+func (cat *Category) Alpha() float64 {
 	return cat.alpha
 }
-func (cat *category) Id() string {
+func (cat *Category) Id() string {
 	return cat.id
 }
 
-// CategoryArgs instances to use as constants
-var (
-	CC_CU     = CategoryArgs{"COPPER", 12000.0, 0.0000169, 0.0, 0.00374, "CU"}
-	CC_AAAC   = CategoryArgs{"AAAC (AASC)", 6450.0, 0.0000230, 20.0, 0.00340, "AAAC"}
-	CC_ACAR   = CategoryArgs{"ACAR", 6450.0, 0.0000250, 20.0, 0.00385, "ACAR"}
-	CC_ACSR   = CategoryArgs{"ACSR", 8000.0, 0.0000191, 20.0, 0.00395, "ACSR"}
-	CC_AAC    = CategoryArgs{"ALUMINUM", 5600.0, 0.0000230, 20.0, 0.00395, "AAC"}
-	CC_CUWELD = CategoryArgs{"COPPERWELD", 16200.0, 0.0000130, 0.0, 0.00380, "CUWELD"}
-	CC_AASC   = CC_AAAC
-	CC_ALL    = CC_AAC
-)
+//----------------------------------------------------------------------------------------
+// *Category instances to use as constants
+//	CC_CU     = &Category{"COPPER", 12000.0, 0.0000169, 0.0, 0.00374, "CU"}
+//	CC_AAAC   = &Category{"AAAC (AASC)", 6450.0, 0.0000230, 20.0, 0.00340, "AAAC"}
+//	CC_ACAR   = &Category{"ACAR", 6450.0, 0.0000250, 20.0, 0.00385, "ACAR"}
+//	CC_ACSR   = &Category{"ACSR", 8000.0, 0.0000191, 20.0, 0.00395, "ACSR"}
+//	CC_AAC    = &Category{"ALUMINUM", 5600.0, 0.0000230, 20.0, 0.00395, "AAC"}
+//	CC_CUWELD = &Category{"COPPERWELD", 16200.0, 0.0000130, 0.0, 0.00380, "CUWELD"}
+//	CC_AASC   = CC_AAAC
+//	CC_ALL    = CC_AAC
+
+var CC_CU, CC_AAAC, CC_ACAR, CC_ACSR, CC_AAC, CC_CUWELD, CC_AASC, CC_ALL *Category
+
+func init() {
+	var err error
+	vc := checker.New("Category Instances as constants")
+
+	CC_CU, err = NewCategory("COPPER", 12000.0, 0.0000169, 0.0, 0.00374, "CU")
+	vc.AppendSub(err)
+	CC_AAAC, err = NewCategory("AAAC (AASC)", 6450.0, 0.0000230, 20.0, 0.00340, "AAAC")
+	vc.AppendSub(err)
+	CC_ACAR, err = NewCategory("ACAR", 6450.0, 0.0000250, 20.0, 0.00385, "ACAR")
+	vc.AppendSub(err)
+	CC_ACSR, err = NewCategory("ACSR", 8000.0, 0.0000191, 20.0, 0.00395, "ACSR")
+	vc.AppendSub(err)
+	CC_AAC, err = NewCategory("ALUMINUM", 5600.0, 0.0000230, 20.0, 0.00395, "AAC")
+	vc.AppendSub(err)
+	CC_CUWELD, err = NewCategory("COPPERWELD", 16200.0, 0.0000130, 0.0, 0.00380, "CUWELD")
+	vc.AppendSub(err)
+	CC_AASC = CC_AAAC
+	CC_ALL = CC_AAC
+
+	if err = vc.Error(); err != nil {
+		panic(err)
+	}
+}
--- conductor.go
+++ conductor.go
@@ -2,11 +2,73 @@
 
 package conductor
 
+import (
+	"fmt"
+
+	"bitbucket.org/tormundo/go.value/checker"
+)
+
+//----------------------------------------------------------------------------------------
+
+// ConductorArgs Container for arguments for Conductor constructor
+// Is a mutable version of a Conductor object that allow to change attributes
+type ConductorArgs struct {
+	Name     string
+	Category *Category
+	Diameter float64
+	Area     float64
+	Weight   float64
+	Strength float64
+	R25      float64
+	Hcap     float64
+	Id       string
+}
+
+// Get Returns *conductor object from attributes values
+func (ca *ConductorArgs) Get() (*Conductor, error) {
+	vc := checker.New(fmt.Sprintf("ConductorArgs Get %s", ca.Name))
+	cond, errsub := NewConductor(ca.Name, ca.Category, ca.Diameter, ca.Area, ca.Weight,
+		ca.Strength, ca.R25, ca.Hcap, ca.Id)
+	vc.AppendSub(errsub)
+	if err := vc.Error(); err != nil {
+		return nil, err
+	}
+	return cond, nil
+}
+
+//----------------------------------------------------------------------------------------
+
+// NewConductor Returns *conductor object from arguments values
+// name     string    : Name of conductor
+// category *category : *category instance
+// diameter float64   : Diameter [mm]
+// area     float64   : Cross section area [mm2]
+// weight   float64   : Weight per unit [kg/m]
+// strength float64   : Rated strength [kg]
+// r25      float64   : Resistance at 25°C [Ohm/km]
+// hcap     float64   : Heat capacity [kcal/(ft*°C)]
+// id       string    : Database id
+func NewConductor(name string, category *Category, diameter float64, area float64, weight float64,
+	strength float64, r25 float64, hcap float64, id string) (*Conductor, error) {
+
+	vc := checker.New(fmt.Sprintf("NewConductor %s", name))
+	vc.Ck("Diameter", diameter).Gt(0.0)
+	vc.Ck("Area", area).Gt(0.0)
+	vc.Ck("Weight", weight).Gt(0.0)
+	vc.Ck("Strength", strength).Gt(0.0)
+	vc.Ck("R25", r25).Gt(0.0)
+	vc.Ck("Hcap", hcap).Gt(0.0)
+	if err := vc.Error(); err != nil {
+		return nil, err
+	}
+	return &Conductor{*category, name, diameter, area, weight, strength, r25, hcap, id}, nil
+}
+
 //----------------------------------------------------------------------------------------
 
 // Conductor Container for conductor characteristics
 type Conductor struct {
-	category         // Category
+	Category         // Category
 	Name     string  // Name of conductor
 	Diameter float64 // Diameter [mm]
 	Area     float64 // Cross section area [mm2]
@@ -15,4 +77,45 @@ type Conductor struct {
 	R25      float64 // Resistance at 25°C [Ohm/km]
 	Hcap     float64 // Heat capacity [kcal/(ft*°C)]
 	Id       string  // Optional database id
+}
+
+// Conductor Container for conductor characteristics
+type conductor struct {
+	name     string    // Name of conductor
+	category *Category // *category instance
+	diameter float64   // Diameter [mm]
+	area     float64   // Cross section area [mm2]
+	weight   float64   // Weight per unit [kg/m]
+	strength float64   // Rated strength [kg]
+	r25      float64   // Resistance at 25°C [Ohm/km]
+	hcap     float64   // Heat capacity [kcal/(ft*°C)]
+	id       string    // Optional database id
+}
+
+func (c *conductor) Name() string {
+	return c.name
+}
+func (c *conductor) Category() *Category {
+	return c.category
+}
+func (c *conductor) Diameter() float64 {
+	return c.diameter
+}
+func (c *conductor) Area() float64 {
+	return c.area
+}
+func (c *conductor) Weight() float64 {
+	return c.weight
+}
+func (c *conductor) Strength() float64 {
+	return c.strength
+}
+func (c *conductor) R25() float64 {
+	return c.r25
+}
+func (c *conductor) Hcap() float64 {
+	return c.hcap
+}
+func (c *conductor) Id() string {
+	return c.id
 }
